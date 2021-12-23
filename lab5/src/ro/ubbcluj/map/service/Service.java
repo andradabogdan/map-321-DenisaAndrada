1ebd9a9fafe6cc05a069362ba91c1e1fa59ce4e3
denisabodnar <denisabodnar24@yahoo.com>
2021-12-23 17:42:38 +0200

    Am adaugat prima functionalitate.

--- lab5/src/ro/ubbcluj/map/service/Service.java
+++ lab5/src/ro/ubbcluj/map/service/Service.java
@@ -1,10 +1,8 @@
 package ro.ubbcluj.map.service;
-
 import ro.ubbcluj.map.domain.*;
 import ro.ubbcluj.map.domain.graph.AbstractGraph;
 import ro.ubbcluj.map.domain.graph.GraphDB;
-import ro.ubbcluj.map.domain.validators.CerereValidator;
-import ro.ubbcluj.map.domain.validators.MesajValidator;
+
 import ro.ubbcluj.map.domain.validators.PrietenieValidator;
 import ro.ubbcluj.map.domain.validators.UtilizatorValidator;
 
@@ -16,14 +14,12 @@ import ro.ubbcluj.map.repository.memory.InMemoryRepository;
 public class Service {
     private final InMemoryRepository<Long, Utilizator> usersRepo;
     private final InMemoryRepository<Long, Prietenie> friendshipsRepo;
-    private final InMemoryRepository<Long, Message> messagesRepo;
-    private final InMemoryRepository<Long, FriendRequest> friendRequestsRepo;
+
 
     public Service(InMemoryRepository<Long, Utilizator> repository, InMemoryRepository<Long, Prietenie> repository2, InMemoryRepository<Long, Message> repository3, InMemoryRepository<Long, FriendRequest> repository4) {
         this.usersRepo = repository;
         this.friendshipsRepo = repository2;
-        this.messagesRepo = repository3;
-        this.friendRequestsRepo = repository4;
+
     }
 
     public void saveUtilizator(String firstName, String lastName) {
@@ -42,46 +38,7 @@ public class Service {
         this.usersRepo.delete(id);
     }
 
-    public void saveMessage(Long from_user, Long to_user, String message, LocalDate data, Long reply) {
-        Message currentMessage = new Message(from_user, to_user, message, data, reply);
-        MesajValidator mesajValidator = new MesajValidator();
-        mesajValidator.validate(currentMessage);
-        List<Message> messages = this.showConversation(from_user,to_user);
-        Message lastMessage = new Message();
-        int i=0;
-        for(Message mesaj: messages){
-            if(i==messages.size()-1){
-                lastMessage = mesaj;
-            }
-            i++;
-        }
-        for(Message mesaj : messages){
-            if(Objects.equals(mesaj.getTo(), from_user) && Objects.equals(mesaj.getFrom(), to_user)
-                    && currentMessage.getData().compareTo(mesaj.getData())<0){
-                throw new IllegalArgumentException("Mesajul adaugat trebuie sa fie cronologic dupa mesajul la care se raspunde!");
-            }
-        }
-        if(Objects.equals(lastMessage.getTo(), currentMessage.getTo())
-                && Objects.equals(lastMessage.getFrom(), currentMessage.getFrom())){
-            currentMessage.setReply(0L);
-        }
-        else{
-            currentMessage.setReply(lastMessage.getId());
-        }
 
-        this.messagesRepo.save(currentMessage);
-    }
-
-    public void deleteMessage(Long id) {
-        Message message = this.messagesRepo.findOne(id);
-        MesajValidator mesajValidator = new MesajValidator();
-        mesajValidator.validate(message);
-        this.messagesRepo.delete(id);
-    }
-
-    public Iterable<Message> findAllMessages() {
-        return this.messagesRepo.findAll();
-    }
 
     public Iterable<Prietenie> findAllFriendships() {
         return this.friendshipsRepo.findAll();
@@ -91,46 +48,6 @@ public class Service {
         return this.usersRepo.findAll();
     }
 
-    public void saveRequests(Long id1, Long id2, String status){
-
-        Iterable<Prietenie> prietenii = this.friendshipsRepo.findAll();
-        for(Prietenie prietenie : prietenii){
-            if((Objects.equals(prietenie.getId1(), id1) && Objects.equals(prietenie.getId2(), id2))
-                    ||(Objects.equals(prietenie.getId1(), id2) && Objects.equals(prietenie.getId2(), id1))){
-                throw new IllegalArgumentException("Cererea nu se poate trimite! Prietenia exista deja!");
-            }
-            if (Objects.equals(id1, id2))
-                throw new IllegalArgumentException("Un utilizator nu se poate sa se imprieteneasca singur!");
-
-
-            FriendRequest friendRequest = new FriendRequest(id1, id2, status);
-            CerereValidator validator = new CerereValidator();
-            validator.validate(friendRequest);
-            this.friendRequestsRepo.save(friendRequest);
-        }
-    }
-
-    public void deleteRequests(Long id) {
-        FriendRequest friendRequest = this.friendRequestsRepo.findOne(id);
-        CerereValidator cerereValidator = new CerereValidator();
-        cerereValidator.validate(friendRequest);
-        this.friendRequestsRepo.delete(id);
-    }
-
-    public void updateStatus(Long id, Status status) {
-        FriendRequest friendRequest = this.friendRequestsRepo.findOne(id);
-        if(status==Status.REJECTED){
-            deleteRequests(id);
-        }
-        else if(status==Status.APPROVED){
-            deleteRequests(id);
-            this.addFriend(friendRequest.getId1(),friendRequest.getId2(),LocalDate.now());
-        }
-    }
-
-    public Iterable<FriendRequest> findAllRequests() {
-        return this.friendRequestsRepo.findAll();
-    }
 
     public void addFriend(Long id1, Long id2, LocalDate data) {
         boolean ok = true;
@@ -216,32 +133,5 @@ public class Service {
                 .filter(filterCriteria).toList();
     }
 
-    public List<Prietenie> showFriendsWithDate(Long userID, int month) {
-        if (month >= 1 && month <= 12) {
-            Predicate<Prietenie> filterCriteria1 = n -> Objects.equals(n.getId1(), userID)
-                    || Objects.equals(n.getId2(), userID);
-            Predicate<Prietenie> filterCriteria2 = n -> n.getData().getMonthValue() == month;
-            Predicate<Prietenie> filterCriteria = filterCriteria1.and(filterCriteria2);
-            List<Prietenie> friendships = toolMethod();
 
-            return friendships.stream()
-                    .filter(filterCriteria).toList();
-        } else {
-            throw new IllegalArgumentException("Luna trebuie sa fie un numar cuprins intre 1 si 12!");
-        }
-    }
-
-    public List<Message> showConversation(Long id1, Long id2) {
-        Iterable<Message> messages = this.messagesRepo.findAll();
-        List<Message> conversation = new ArrayList<>();
-        for(Message message : messages){
-            if((Objects.equals(message.getFrom(), id1) && Objects.equals(message.getTo(), id2))
-                    ||(Objects.equals(message.getFrom(), id2) && Objects.equals(message.getTo(), id1))){
-                conversation.add(message);
-            }
-        }
-        Comparator<Message> mapComparator = (Message m1, Message m2) -> m1.getData().compareTo(m2.getData());
-        conversation.sort(mapComparator);
-        return conversation;
-    }
 }
